dbebce41749f680ca3bf83739e1f6f33791295f2
Sally Hasselbrook <shasselbrook@earthlink.net>
2013-02-27 09:35:28 -0500

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
   
-  get "static_pages/about"
+  root to: "static_pages#home"
+  
+  #get "static_pages/home"
+  match '/home', to: 'static_pages#home'
 
-  get "static_pages/contact"
+  #get "static_pages/help"
+  match '/help', to: 'static_pages#help'
+  
+  #get "static_pages/about"
+  match '/about', to: 'static_pages#about'
+
+  #get "static_pages/contact"
+  match '/contact', to: 'static_pages#contact'
+
+  get "users/new"
+  match 'signup', to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
