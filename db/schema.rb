791c0b2be079caafff1c0c796ab2c32537c22045
Sally Hasselbrook <shasselbrook@earthlink.net>
2013-03-05 15:07:42 -0500

    readd password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130228142613) do
+ActiveRecord::Schema.define(:version => 20130305200411) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
