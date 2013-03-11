b69c6705f2b3c01e141dc020804672122f8c2e03
Sally Hasselbrook <shasselbrook@earthlink.net>
2013-03-11 16:05:12 -0400

    Finish user edit, update, index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,25 +1,46 @@
 module SessionsHelper
   
+  # sign in the user and remember them with a cookie
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
     self.current_user = user
   end
   
+  # check if current_user is signed in
   def signed_in?
     !current_user.nil?
   end
-  # like a set property definition
+  
+  # set the current user
   def current_user=(user)
     @current_user = user
   end
   
-  # like a get property
+  # get the current user
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
   
+  # check if user passed in is the current user as set above
+  def current_user?(user)
+    user == current_user
+  end
+  
+  # sign out user and delete cookie that remembers them
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
   end
+
+  # save current location
+  def store_location
+    session[:return_to] = request.url
+  end
+  
+  # redirect to saved location or default
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+  
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -9,14 +9,30 @@ def full_title(page_title)
   end
 end
 
-def valid_signin(user)
+def sign_in(user)
+  visit signin_path
   fill_in "Email", with: user.email.upcase
   fill_in "Password", with: user.password
   click_button "Sign in"
+  # sign in when not using Capybara as well
+  cookies[:remember_token] = user.remember_token
+end
+
+def enter_valid_signup(email)
+    fill_in "Name", with: "Example User"
+    fill_in "Email", with: email
+    fill_in "Password", with: "foobar"
+    fill_in "Confirmation", with: "foobar"
 end
 
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
   end
 end
